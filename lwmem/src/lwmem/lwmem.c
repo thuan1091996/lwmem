94b794d323c9ebaf11961ff456b612cca0e1834a
thuantm <minhthuan1091996@gmail.com>
2023-02-24 09:45:22 +0700

    Fix mutex implemtations

--- lwmem/src/lwmem/lwmem.c
+++ lwmem/src/lwmem/lwmem.c
@@ -158,19 +158,19 @@ uint8_t lwmem_sys_mutex_isvalid(LWMEM_CFG_OS_MUTEX_HANDLE* m)
 {
     if (NULL != m)
     {
-        return m->handle != NULL;
+        return NULL != m->handle;
     }
     return 0;
 }
 
 uint8_t
 lwmem_sys_mutex_wait(LWMEM_CFG_OS_MUTEX_HANDLE* m) {
-    return chip_os_mutex_give(m) == CHIP_OS_OK;
+    return CHIP_OS_OK == chip_os_mutex_take(m, CHIP_OS_TIME_FOREVER);
 }
 
 uint8_t
 lwmem_sys_mutex_release(LWMEM_CFG_OS_MUTEX_HANDLE* m) {
-    return chip_os_mutex_take(m, CHIP_OS_TIME_FOREVER) == CHIP_OS_OK;
+    return chip_os_mutex_give(m) == CHIP_OS_OK;
 }
 
 
